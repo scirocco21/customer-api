8e95ce009753a61846adb2589b510beaba340581
Sebastian <sebgertz@gmail.com>
2019-04-08 22:49:32 -0400

    feat: controller returns appropriate status codes and messages

--- api/controllers/customer_api.js
+++ api/controllers/customer_api.js
@@ -1,50 +1,49 @@
 'use strict';
 // Include mock "db"
 const db = require('../../config/mock_db')();
-// Exports all the functions to perform on the db
-module.exports = {save, getOne, update, getByPhoneNumber};
+// Make all the functions performed on the db accessible as operationIds in swagger.yaml file
+module.exports = { save, getOne, update, getByPhoneNumber };
 
-//POST /customers operationId
 function save(req, res, next) {
-    res.json({success: db.save(req.body), description: "Movie added to the list!"});
+  const exitCode = db.save(req.body)
+  if (exitCode === 1) {
+    res.status(201).json({ description: "New customer added to database!" });
+  } else if (exitCode === 2) {
+    res.status(400).json({ description: "A user with this email address already exists" })
+  } else if (exitCode === 0) {
+    res.status(400).json({ description: "Invalid Input. Try again" })
+  }
 }
 
 function getByPhoneNumber(req, res, next) {
-    let customers = db.find();
-    let phoneNumber = req.swagger.params.phoneNumber.value;
-    console.log(phoneNumber);
-    let results = customers.filter(customer => {
-        return customer.phoneNumber == phoneNumber
-    })
-    console.log(results)
-    if(results.length !== 0) {
-        res.json(results);
-    } else {
-        res.status(204).send();
-    }
+  const customers = db.find();
+  const phoneNumber = req.swagger.params.phoneNumber.value.replace(/\D/g, '');
+  const results = customers.filter(customer => {
+    return customer.phoneNumber == phoneNumber
+  })
+  if (results.length !== 0) {
+    res.status(200).json(results);
+  } else {
+    res.status(404).json({ description: "Could not find a customer with that number" });
+  }
 }
 
-//GET /movie/{id} operationId
 function getOne(req, res, next) {
-    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
-    var customer = db.find(id);
-    if(customer) {
-        res.json(customer);
-    }else {
-        res.status(204).send();
-    }        
+  const id = req.swagger.params.id.value;
+  const customer = db.find(id);
+  if (customer) {
+    res.status(200).json(customer);
+  } else {
+    res.status(404).json({ description: "Could not find a customer with that id" });
+  }
 }
-//PUT /movie/{id} operationId
+
 function update(req, res, next) {
-    var id = req.swagger.params.id.value; //req.swagger contains the path parameters
-    console.log(id);
-    var phoneNumber = req.body.phoneNumber;
-    console.log(req.body.phoneNumber)
-    if(db.update(id, phoneNumber)){
-        console.log("I was hit")
-        res.json({success: 1, description: "Phone number updated!"});
-        console.log(res)
-    }else{
-        res.status(204).send();
-    }
+  const id = req.swagger.params.id.value;
+  const phoneNumber = req.body.phoneNumber;
+  if (db.update(id, phoneNumber)) {
+    res.status(200).json({ description: "Phone number updated!" });
+  } else {
+    res.status(400).json({ description: "Something went wrong. Did you enter a valid phone number?" });
+  }
 }
