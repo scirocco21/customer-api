aa9f8b5aa9a198c10ccf22fe6f64d0457d6e988d
Sebastian <sebgertz@gmail.com>
2019-04-08 22:50:57 -0400

    feat: customer model contains all required fields

--- api/models/Customer.js
+++ api/models/Customer.js
@@ -1,9 +1,11 @@
 class Customer {
     constructor(customer) {
+        this.id = customer.id;
         this.name = customer.name;
         this.email = customer.email;
         this.address = customer.address;
         this.phoneNumber = customer.phoneNumber;
+        this.dob = customer.dob;
     }
 }
 
